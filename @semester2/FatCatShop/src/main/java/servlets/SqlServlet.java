eb254e8e86c8caa8e488daf1bdfb6f48c4ea4b94
Dimitar Zurnov <mitkozurnov@gmail.com>
2021-07-10 22:37:27 +0300

    Creating custom Query Builder based on JDBC

---
+++ @semester2/FatCatShop/src/main/java/servlets/SqlServlet.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.mysql.jdbc.Driver;
+public class SqlServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//Ръчно зареждане на двайвера на MySQL
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		// Връзка към базата
+		try {
+			Connection databaseConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javadb","root","");
+			System.out.println("Success connection");
+		// Съвздаване на SQL statement
+			Statement queryManager = databaseConnection.createStatement();
+		// Изпълнение на заявката
+			ResultSet collection = queryManager.executeQuery("SELECT first_name, last_name\r\n"
+					+ "FROM users");
+			
+			while(collection.next()) {
+				String firstName = collection.getString("first_name");
+				String lastName = collection.getString("last_name");
+				
+				System.out.print(firstName + lastName);
+				System.out.print("*");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
