00da64dcf7516d178b4cb63211fb998f1ebc0168
Dimitar Zurnov <mitkozurnov@gmail.com>
2021-11-17 21:50:12 +0200

    Added Auth

--- @semester2/trelloclone/src/main/java/com/trelloclone/trelloclone/TrellocloneApplication.java
+++ @semester2/trelloclone/src/main/java/com/trelloclone/trelloclone/TrellocloneApplication.java
@@ -9,5 +9,4 @@ public class TrellocloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrellocloneApplication.class, args);
 	}
-
 }
--- @semester2/trelloclone/src/main/java/com/trelloclone/trelloclone/controllers/DashboardController.java
+++ @semester2/trelloclone/src/main/java/com/trelloclone/trelloclone/controllers/DashboardController.java
@@ -1,7 +1,8 @@
 package com.trelloclone.trelloclone.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class DashboardController {
     @GetMapping("/dashboard")
     public String getDashboardPage(){
